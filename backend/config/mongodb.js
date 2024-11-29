477bed44c15187b086cdf8551a9d75d8afae15eb
kellishbond <kellishbond@gmail.com>
2024-11-29 17:01:47 +0100

    backend

---
+++ backend/config/mongodb.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+    mongoose.connection.on('connected', () => {
+        console.log('DB Connected');
+    });
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`, {
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
+    }); // Correctly closed the object and statement
+};
+
+
+export default connectDB;
